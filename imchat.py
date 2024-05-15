287a527150aa26b90fa0b0d65699d5e53c61c41a
tom wawer <sq5rix@gmail.com>
2024-05-15 13:57:44 +0200

    infer z listy

--- imchat.py
+++ imchat.py
@@ -16,10 +16,8 @@ def infer_chat(prompt):
     """
 
     load_dotenv()
-
     api_key = os.getenv("OPENAI_KEY")
     openai.api_key = api_key
-
     response = openai.chat.completions.create(
         model=GPT_MODEL,
         messages=[
@@ -31,9 +29,29 @@ def infer_chat(prompt):
     )
     return response.choices[0].message.content
 
+def infer_from_list(prompt_list):
+    """
+    Generates text based on the given prompt using OpenAI's GPT-4 API.
+
+    Args:
+    prompt (str): The prompt to generate text from.
+
+    Returns:
+    str: The generated text.
+    """
+
+    load_dotenv()
+    api_key = os.getenv("OPENAI_KEY")
+    openai.api_key = api_key
+    response = openai.chat.completions.create(
+        model=GPT_MODEL,
+        messages=[{ "role": "user", "content": prompt } for prompt in prompt_list ],
+    )
+    return response.choices[0].message.content
+
 def main():
     #generate_text = infer_chat(f"Treść: {read_all_files()} Pytanie: {general_prompt}")
-    e = infer_chat('hello what is a conundrum?')
+    e = infer_from_list(['as a linguist', 'what is a conundrum?', 'give me a original conundrum example','give the answer'])
     print(e)
 
 if __name__ == "__main__":
