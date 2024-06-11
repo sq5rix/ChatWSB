5092b8dc2e34d92384b87b52c492c7683e2d8e83
Tom <sq5rix@gmail.com>
2024-06-11 23:58:37 +0200

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -26,11 +26,12 @@ ocen = "oceń w skali 1-5,bez opisu, samą liczbą, "
 
 wstep = "Jako surowy nauczyciel akademicki oceń kolowium, podane poniżej, na temat: "
 
-ocen_strukture = f"{ocen}, czy struktura kolokwium jest poprawna"
+ocen_strukture = f"{ocen}, czy struktura kolokwium jest poprawna, czyli czy glowne punkty prawidlowo opisuja odpowiedz na pytanie. Wygeneruj wlasne glowne punkty i sprawdz, czy są równoważne z punktami z odpowiedzi. Jesli nue na punktow, tylko sam opis ocen strukturę na 1. "
 ocen_punkty = f"{ocen} czy punkty wyczerpują temat w odpowiedzi"
-ocen_zawartosc_merytoryczna = f"{ocen} zawartość merytoryczną punktów, czy wyczerpują temat z pytania "
+ocen_zawartosc_merytoryczna = f"{ocen} zawartość merytoryczną punktów, czy kazdy z opisow pod glownymi punktami jest obszerny i opisuje glowne punkty wyczerpują temat z pytania. jesli bie ma odpowiedzi punkt-opis sprawdź czy odpowiedz jest obszerna i wtczerpujaca. porownaj ją z odpowiedzią, jakiej sam bys udzielił. "
 ocen_odnosniki = f"{ocen} czy są odnośniki i linki do zewnętrznych źródeł informacji, takich jak artykuły, strony web, repozytoria gitub w:"
-ocen_samodzielnosc = f"{ocen}, czy kolokwium zostało napisane samodzielnie, 1 jeśli jest to kopia ChatGPT, 5 jeśli całkowicie samodzielnie."
+ocen_samodzielnosc = f"{ocen}, czy kolokwium zostało napisane samodzielnie, 1 jeśli jest to dokladna kopia odpowiedzi, jaką sam bys udzielil, 5 jeśli jest zupelnie inna."
+ocen_zrozumienie = f"{ocen} czy student rozumie to co napisal. sprobuj  mznienuc firmat oytania i dorawdz, czy odpowiedz pasuje do znueniinej formy pytania. "
 parametry_oceny = f"oceny podaj w formacie: str:[ocena struktury], pun[ocena punktow], zaw[ocena zawartości], odn[ocena odnosników], sam[ocena samodzielności]. Nie uzywaj znakow końca linii tylko przecinków"
 
 def ocena_kolokwium(pyt, odp, zrodla):
@@ -41,6 +42,7 @@ def ocena_kolokwium(pyt, odp, zrodla):
     {ocen_punkty}
     {ocen_zawartosc_merytoryczna}
     {ocen_odnosniki}{zrodla}
+    {ocen_zrozumienie}
     {ocen_samodzielnosc}
     {parametry_oceny}
     Oceń poniższe kolokwium: {odp}
